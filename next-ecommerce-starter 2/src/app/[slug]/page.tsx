a2fbf3dac3b3cbd34b42a106a4c999746e890314
Jade Alvares <jadea@webother.com>
2024-07-24 11:51:52 +0100

    added navbar and gooter to home and a list page at /list

---
+++ next-ecommerce-starter 2/src/app/[slug]/page.tsx
@@ -0,0 +1,12 @@
+// pages/list.js
+import React from 'react';
+
+const SinglePage = () => {
+  return (
+    <div>
+      <h1>List Page</h1>
+    </div>
+  );
+};
+
+export default SinglePage;
---
+++ next-ecommerce-starter 2/src/app/list/page.tsx
@@ -0,0 +1,12 @@
+// pages/list.js
+import React from 'react';
+
+const ListPage = () => {
+  return (
+    <div>
+      <h1>List Page</h1>
+    </div>
+  );
+};
+
+export default ListPage;
